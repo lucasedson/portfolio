498dd719be4060438fa69a0b6b88741ecc2eec5a
Lucas Edson <lucasedson654@gmail.com>
2024-08-08 16:59:14 -0300

    Initial + Hero

--- portfolio/src/components/Hero/Hero.tsx
+++ portfolio/src/components/Hero/Hero.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
     let bglink = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK3MujE_8A9TDyTzM-nUFt_ef9Qp6ytrb29A&s";
     return (
-        <div className=" flex flex-1 w-full h-screen text-slate-300  justify-center">
-            <img src={bglink} className="w-screen h-screen object-cover absolute -z-10 opacity-10" />
+        <section className=" flex flex-1 w-full h-[95vh] text-slate-300  justify-center">
+            <img src={bglink} className="w-full h-[100vh] object-cover absolute -z-10 opacity-10 blur-sm" />
             <div className="ml-100 mr-100 mt-20 flex flex-col items-center max-w-5xl gap-28 md:flex-row ">
                 
                 <div className="flex flex-col  items-center w-[300px]">
@@ -16,7 +16,7 @@ export default function Hero() {
                 </div>
 
                 <div className="flex text-center flex-col text-wrap w-[300px]">
-                    <h1 className="text-justify text-2xl font-bold mb-2">Desenvolvedor Full-Stack de Cabo Frio, RJ, Brasil</h1>
+                    <h1 className="text-justify text-2xl font-bold mb-2">Desenvolvedor Full-Stack! Cabo Frio, RJ, Brasil</h1>
                     
                     <p className="text-justify">
                     Estou disponível para trabalhos sob demanda, oferecendo uma combinação de experiência em 
@@ -32,6 +32,6 @@ export default function Hero() {
                 </div>
 
             </div>
-        </div>
+        </section>
     );
 }
