17dd44dd6216bf31161bc6d6b9ff4d4a0af7f0a6
Lucas Edson <lucasedson654@gmail.com>
2025-07-11 12:19:00 -0300

    Cria animação de transição

--- portifolio/deploy.js
+++ portifolio/deploy.js
@@ -1,3 +1,4 @@
 import fs from "fs";
 // Copia todos os arquivos  e pastas da pasta portfolio/dist e cola no .
 fs.cpSync("./dist", "../", { recursive: true });
+console.log("Arquivos copiados com sucesso!");
